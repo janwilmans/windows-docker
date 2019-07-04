aaa156e2474e502d64a1c28316fdd215256f246b
Jan Wilmans <jan.wilmans@kindtechnologies.nl>
2019-07-04 11:52:32 +0200

    added nsis

--- autoinstall_vsbuildtools/2_install_apps_as_admin.bat
+++ autoinstall_vsbuildtools/2_install_apps_as_admin.bat
@@ -7,6 +7,7 @@ choco install -y cppcheck
 choco install -y nodejs --version 8.11.3
 choco install -y notepadplusplus
 choco install -y gitlab-runner
+choco install -y nsis.install
 :: choco install -y docker-desktop
 choco install -y wget
 ::choco install -y gnuwin32-coreutils.install
