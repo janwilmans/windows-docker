dd0622d2201bd74c9922e0ed023577ea1b890a4c
Jan Wilmans <jan.wilmans@kindtechnologies.nl>
2019-07-04 11:57:17 +0200

    added update packages

--- autoinstall_vsbuildtools/2_install_apps_as_admin.bat
+++ autoinstall_vsbuildtools/2_install_apps_as_admin.bat
@@ -1,18 +1,7 @@
 :: run this as administrator
 
-::goto skip_choco
-choco install -y git
-choco install -y python3
-choco install -y cppcheck
-choco install -y nodejs --version 8.11.3
-choco install -y notepadplusplus
-choco install -y gitlab-runner
-choco install -y nsis.install
-:: choco install -y docker-desktop
-choco install -y wget
-::choco install -y gnuwin32-coreutils.install
+call update_packages.bat
 
-:skip_choco
 mkdir c:\temp 2>NUL
 wget https://aka.ms/vs/16/release/channel -O c:\temp\VisualStudio.chman
 wget https://aka.ms/vs/16/release/vs_buildtools.exe -O c:\temp\vs_buildtools.exe
---
+++ autoinstall_vsbuildtools/update_packages.bat
@@ -0,0 +1,10 @@
+choco install -y git
+choco install -y python3
+choco install -y cppcheck
+choco install -y nodejs --version 8.11.3
+choco install -y notepadplusplus
+choco install -y gitlab-runner
+choco install -y nsis.install
+choco install -y wget
+:: choco install -y docker-desktop
+:: choco install -y gnuwin32-coreutils.install
