d1cfab2bec8d5716797b76bba467364d3ead6dcd
Jan Wilmans <jan.wilmans@kindtechnologies.nl>
2019-07-09 13:44:30 +0200

    Fixed silent install for visal studio

---
+++ win10-image/vs_professional_2019.bat
@@ -0,0 +1,15 @@
+cd %~dp0
+vs_professional_2019.exe --passive --add Microsoft.VisualStudio.Workload.ManagedDesktopBuildTools ^
+    --add Microsoft.Net.Component.3.5.DeveloperTools ^
+    --add Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools ^
+    --add Microsoft.Net.ComponentGroup.TargetingPacks.Common ^
+    --add Microsoft.VisualStudio.Component.TestTools.BuildTools ^
+    --add Microsoft.VisualStudio.Workload.VCTools ^
+    --add Microsoft.VisualStudio.Component.VC.140 ^
+    --add Microsoft.VisualStudio.Component.VC.ATL ^
+    --add Microsoft.VisualStudio.Component.VC.CLI.Support ^
+    --add Microsoft.VisualStudio.Component.Windows10SDK.17763 ^
+    --add Microsoft.VisualStudio.Component.WinXP ^
+    --add Microsoft.VisualStudio.Component.JavaScript.TypeScript ^
+    --includeRecommended
+
