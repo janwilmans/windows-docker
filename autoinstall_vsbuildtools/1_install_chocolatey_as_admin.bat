d644f670a579a104ec2c3010c1736468ddbdd2c3
Jan Wilmans <jan.wilmans@kindtechnologies.nl>
2019-07-09 15:48:32 +0200

    move installation of vs into separate batch files and re-added commandline configuration

--- autoinstall_vsbuildtools/1_install_chocolatey_as_admin.bat
+++ autoinstall_vsbuildtools/1_install_chocolatey_as_admin.bat
@@ -1,6 +1,12 @@
 :: run this as administrator
 pushd %~dp0\
+
+:: build server settings/tweaks 
 regedit /s developer_settings.reg
+sc config WSearch start=disabled
+net stop WSearch
+
+:: install chocolatey
 powershell -command Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 @echo *****
 @echo Now close this window and run '2_install_apps_as_admin.bat' as administrator
--- autoinstall_vsbuildtools/2_install_apps_as_admin.bat
+++ autoinstall_vsbuildtools/2_install_apps_as_admin.bat
@@ -3,28 +3,7 @@
 :: make script directory the current directory
 pushd %~dp0
 call update_packages.bat
-
-mkdir c:\temp 2>NUL
-wget https://aka.ms/vs/16/release/channel -O c:\temp\VisualStudio.chman
-wget https://aka.ms/vs/16/release/vs_buildtools.exe -O c:\temp\vs_buildtools.exe
-
-c:\temp\vs_buildtools.exe --quiet --wait --norestart --nocache ^
-    --channelUri C:\TEMP\VisualStudio.chman ^
-    --installChannelUri C:\TEMP\VisualStudio.chman ^
-    --add Microsoft.VisualStudio.Workload.ManagedDesktopBuildTools ^
-    --add Microsoft.Net.Component.3.5.DeveloperTools ^
-    --add Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools ^
-    --add Microsoft.Net.ComponentGroup.TargetingPacks.Common ^
-    --add Microsoft.VisualStudio.Component.TestTools.BuildTools ^
-    --add Microsoft.VisualStudio.Workload.VCTools ^
-    --add Microsoft.VisualStudio.Component.VC.140 ^
-    --add Microsoft.VisualStudio.Component.VC.ATL ^
-    --add Microsoft.VisualStudio.Component.VC.CLI.Support ^
-    --add Microsoft.VisualStudio.Component.Windows10SDK.16299.Desktop ^
-    --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.WinXP ^
-    --add Microsoft.VisualStudio.Component.TypeScript.2.8 ^
-    --includeRecommended ^
-    --installPath C:\BuildTools
+call vs_professional_2019.bat
 
 @echo -----------
 @echo Execute these manual steps:
