a132ab0cfdc601d525104eef837ad1bb1336971f
Jan Wilmans <jan.wilmans@kindtechnologies.nl>
2019-07-30 09:39:45 +0200

    added choco install vcredist-all

--- autoinstall_vsbuildtools/update_packages.bat
+++ autoinstall_vsbuildtools/update_packages.bat
@@ -6,5 +6,7 @@ choco install -y notepadplusplus
 choco install -y gitlab-runner
 choco install -y nsis.install
 choco install -y wget
+choco install -y vcredist-all
+
 :: choco install -y docker-desktop
 :: choco install -y gnuwin32-coreutils.install
